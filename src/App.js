b023bc8a8d5360a45ea3b14ce4929f3bdb1e2e29
mohamedthamizullah2024 <sthamiz2024@gmail.com>
2024-05-10 07:40:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import ImageGenerator from './Components/ImageGenerator/ImageGenerator';
+
+function App() {
+  return (
+    <ImageGenerator />
+  );
+}
+
+export default App;
